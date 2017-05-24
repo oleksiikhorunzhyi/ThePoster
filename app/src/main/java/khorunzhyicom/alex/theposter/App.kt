83bad4c6c0690978423773e159dc73d947577213
Alex Khorunzhy <alex.kh@techery.io>
2017-05-24 15:14:52 +0300

    Kapt integrations

---
+++ app/src/main/java/khorunzhyicom/alex/theposter/App.kt
@@ -0,0 +1,34 @@
+package khorunzhyicom.alex.theposter
+
+import android.app.Application
+
+import khorunzhyicom.alex.theposter.di.components.ApiComponent
+import khorunzhyicom.alex.theposter.di.components.AppComponent
+import khorunzhyicom.alex.theposter.di.components.DaggerAppComponent
+import khorunzhyicom.alex.theposter.di.components.UiComponent
+import khorunzhyicom.alex.theposter.di.modules.ApiModule
+import khorunzhyicom.alex.theposter.di.modules.AppModule
+import khorunzhyicom.alex.theposter.di.modules.UiModule
+
+class App : Application() {
+
+    lateinit var appComponent: AppComponent
+    lateinit var apiComponent: ApiComponent
+    lateinit var uiComponent: UiComponent
+
+    override fun onCreate() {
+        super.onCreate()
+        this.appComponent = DaggerAppComponent.builder().appModule(AppModule(this)).build()
+        this.apiComponent = appComponent.plus(ApiModule())
+        this.uiComponent = apiComponent.plus(UiModule())
+        this.appComponent.inject(this)
+    }
+
+    fun apiComponent(): ApiComponent {
+        return apiComponent
+    }
+
+    fun uiComponent(): UiComponent {
+        return uiComponent
+    }
+}
--- app/src/main/java/khorunzhyicom/alex/theposter/di/modules/ApiConfigModule.java
+++ app/src/main/java/khorunzhyicom/alex/theposter/di/modules/ApiConfigModule.java
@@ -5,9 +5,11 @@ import dagger.Provides;
 import khorunzhyicom.alex.theposter.api.auth.AuthData;
 import khorunzhyicom.alex.theposter.api.auth.AuthDataProvider;
 import khorunzhyicom.alex.theposter.api.auth.ImmutableAuthData;
+import khorunzhyicom.alex.theposter.api.auth.ImmutableAuthDataProvider;
 import khorunzhyicom.alex.theposter.api.config.ConfigData;
 import khorunzhyicom.alex.theposter.api.config.ConfigDataProvider;
 import khorunzhyicom.alex.theposter.api.config.ImmutableConfigData;
+import khorunzhyicom.alex.theposter.api.config.ImmutableConfigDataProvider;
 import khorunzhyicom.alex.theposter.di.scoupes.ServiceScope;
 
 @Module
@@ -16,16 +18,16 @@ public class ApiConfigModule {
     @ServiceScope
     @Provides
     AuthDataProvider<AuthData> provideAuthDataProvider() {
-        return () -> ImmutableAuthData.builder()
+        return ImmutableAuthDataProvider.builder().provide(ImmutableAuthData.builder()
                 .token("c4b8ea2605abcab92ec0ca2c0d9b864e")
-                .build();
+                .build()).build();
     }
 
     @ServiceScope
     @Provides
     ConfigDataProvider<ConfigData> provideConfigDataProvider() {
-        return () -> ImmutableConfigData.builder()
+        return ImmutableConfigDataProvider.builder().provide(ImmutableConfigData.builder()
                 .url("https://api.themoviedb.org/")
-                .build();
+                .build()).build();
     }
 }
