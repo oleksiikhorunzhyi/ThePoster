43474100a4f3f3e6b4c428498e9d76e2e562b7d6
Alex Khorunzhy <alex.kh@techery.io>
2017-06-21 12:48:37 +0300

    Remove appcomponent

--- app/src/main/java/khorunzhyicom/alex/theposter/di/components/ApplicationComponent.kt
+++ app/src/main/java/khorunzhyicom/alex/theposter/di/components/ApplicationComponent.kt
@@ -11,7 +11,7 @@ import javax.inject.Singleton
 
 
 @Singleton
-@Component(modules = arrayOf(AppModule::class, AndroidInjectionModule::class, InitializerModule::class, InteractorsModule::class, ApiConfigModule::class, JanetModule::class, ConverterModule::class))
+@Component(modules = arrayOf(AndroidInjectionModule::class, InitializerModule::class, InteractorsModule::class, ApiConfigModule::class, JanetModule::class, ConverterModule::class))
 interface ApplicationComponent : AndroidInjector<App>, CommandInjector, InitializerInjector {
 
     @Component.Builder abstract class Builder : AndroidInjector.Builder<App>()
--- app/src/main/java/khorunzhyicom/alex/theposter/di/modules/AppModule.kt
+++
@@ -1,7 +0,0 @@
-package khorunzhyicom.alex.theposter.di.modules
-
-import dagger.Module
-import khorunzhyicom.alex.theposter.App
-
-@Module
-class AppModule(var app: App)
